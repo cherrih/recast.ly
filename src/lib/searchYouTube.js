383e2ef8d235892a4253851992cf21cafb2fd646
cherrih <cherri.hartigan@gmail.com>
2018-11-27 12:30:19 -0800

    partial get request implementation

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,10 @@
 var searchYouTube = (options, callback) => {
   // TODO
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/channels?part=contentDetails&mine=true',
+    
+  
+  });
 };
 
 export default searchYouTube;
